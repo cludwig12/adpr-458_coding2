fb48e02405ad99a4415350e5326b7f99b3e57589
chloe ludwig <cludwig12@husekrs.unl.edu>
2025-03-27 10:55:28 -0500

    in-class exercise 10

--- Assignments/js/script.js
+++ Assignments/js/script.js
@@ -2,6 +2,6 @@ const toggleBtn = document.getElementById("toggle-button");
 const navlistItems = document.getElementById (nav-menu-list);
 console.log("Howdy1")
 
-toggleBtn.addEventListener('click', () => () {
-    navlistItems.classList.toggle('active');
-})
+toggleBtn.addEventListener('click',function () {
+    navlistItems.classList.toggle("active");
+});
