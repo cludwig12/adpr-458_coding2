47c0c787bcce982bc3bfc27f10896908b48d0302
chloe ludwig <cludwig12@husekrs.unl.edu>
2025-04-01 11:04:00 -0500

    preparing for project 2

--- Assignments/js/script.js
+++ Assignments/js/script.js
@@ -1,6 +1,5 @@
 const toggleBtn = document.getElementById("toggle-button");
-const navlistItems = document.getElementById (nav-menu-list);
-console.log("Howdy1")
+const navlistItems = document.getElementById("nav-menu-list");
 
 toggleBtn.addEventListener('click',function () {
     navlistItems.classList.toggle("active");
