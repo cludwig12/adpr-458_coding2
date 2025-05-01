26e38f48470f6186d9d793130dfe7b451d2e90c3
chloe ludwig <cludwig12@husekrs.unl.edu>
2025-05-01 10:56:33 -0500

    put in slider

--- final_project/js/script.js
+++ final_project/js/script.js
@@ -4,3 +4,12 @@ const navlistItems = document.getElementById("nav-menu-list");
 toggleBtn.addEventListener('click',function () {
     navlistItems.classList.toggle("active");
 });
+
+$('.slider').slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    adaptiveHeight: true,
+    autoplay: true
+});
